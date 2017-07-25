694f96a05d923817524ae375040bd00afd69cfc0
Sonam Jaiswal <sonam0211@gmail.com>
2017-07-25 20:01:34 +0530

    app modified

--- app.js
+++ app.js
@@ -40,6 +40,8 @@ var budgetController = (function() {
                 newItem = new Expense(ID, des, val);
 
             }
+            data.allItems[type].push(newItem);
+            return newItem;
 
         },
 
@@ -56,7 +58,9 @@ var UIController = (function() {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
     }
 
     return {
@@ -69,6 +73,35 @@ var UIController = (function() {
             
                 };
             },
+        addListItem:function(obj,type) {
+            var html, newHtml, element;
+            if (type === 'inc'){
+                element = DOMstring.incomeContainer;
+                  html = '<div class="item clearfix" id="inc-%id%"> <div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            } else if (type === 'exp') {
+                element = DOMstring.expensesContainer;
+                
+                html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+            
+            // Insert the HTML into the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+            
+
+        },
+        clearField:function() {
+            var fields, fieldsArr;
+            fields = document.querySelectorAll(DOMstring.inputDescription +','+ DOMstring.inputValue);
+            fieldsArr = Array.prototype.slice.call(fields);
+            fieldsArr.forEach(function(current, index, array){
+                current.value = "";
+            });
+
+        },
         
         getDOMstring:function() {
             return DOMstring;
@@ -101,6 +134,8 @@ var appController = (function(budgetCtrl,UICtrl) {
         var input, newItem;        
         input = UICtrl.getInput();
         newItem = budgetCtrl.addItem(input.type,input.description,input.value);
+        UICtrl.addListItem(newItem,input.type);
+        UICtrl.clearField();
 
     };
 
